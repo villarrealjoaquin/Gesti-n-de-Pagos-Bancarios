4c12ec4e4770f3ab3a1c5bb4c8b98192be3302b7
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2024-04-11 19:01:47 -0300

    testing render

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -15,6 +15,9 @@ class UserController {
       }));
       return res.status(HttpStatus.OK).json(dataWithoutPassword);
     } catch (error) {
+      console.log(error);
+      console.log("Error getting users testing");
+      
       return res
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
         .json({ message: "Error getting users" });
