ccc450f4aa948686a0fbc5bd72db25481e082254
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2024-04-11 19:58:53 -0300

    Update backend URL configuration

--- client/src/services/axios.config.ts
+++ client/src/services/axios.config.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://gestion-de-pagos-bancarios.onrender.com/api",
   withCredentials: true,
 });
 
 export default instance;
-
-// https://challenge-4tmy.onrender.com/api
